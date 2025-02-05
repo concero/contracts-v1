842691c5c7aecff1597b194eaa8f740418604bc7
Aleksei Gerasev <alexey@concero.io>
2025-02-05 19:28:34 +0300

    add _setFees

--- test/foundry/Bridge/BridgeTestFees.t.sol
+++ test/foundry/Bridge/BridgeTestFees.t.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.20;
 import {Test, console} from "forge-std/src/Test.sol";
 import {ConceroBridgeMock} from "test/foundry/Mocks/ConceroBridgeMock.sol";
 import {IInfraStorage} from "contracts/Interfaces/IInfraStorage.sol";
-import {CHAIN_SELECTOR_BASE, CHAIN_SELECTOR_POLYGON} from "contracts/Constants.sol";
+import {CHAIN_SELECTOR_BASE, CHAIN_SELECTOR_OPTIMISM, CHAIN_SELECTOR_POLYGON, CHAIN_SELECTOR_ARBITRUM} from "contracts/Constants.sol";
 import {IInfraOrchestrator} from "contracts/Interfaces/IInfraOrchestrator.sol";
 import {InfraOrchestratorMock} from "../Mocks/InfraOrchestratorMock.sol";
 import {DeployInfraScript} from "../scripts/DeployInfra.s.sol";
@@ -23,24 +23,26 @@ contract BridgeTestFees is Test {
         infraProxy = InfraOrchestratorMock(payable(deployInfraScript.run(forkId)));
         conceroBridge = deployInfraScript.conceroBridgeMock();
 
-        _setFees(CHAIN_SELECTOR_POLYGON);
-        _setFees(CHAIN_SELECTOR_BASE);
+        _setFeesBase(CHAIN_SELECTOR_BASE);
+        _setFeesArb(CHAIN_SELECTOR_ARBITRUM);
+        _setFeesOpt(CHAIN_SELECTOR_OPTIMISM);
+        _setFeesPolygon(CHAIN_SELECTOR_OPTIMISM);
         _;
     }
 
     function test_ConceroBridgeFeesEqual()
         public
-        selectForkAndUpdateInfraProxy(vm.createFork(vm.envString("BASE_RPC_URL"), 23814899))
+        selectForkAndUpdateInfraProxy(vm.createFork(vm.envString("BASE_RPC_URL"), 25989254))
     {
         uint256 amount = 10 * USDC_DECIMALS;
         uint256 oldFees = infraProxy.getSrcTotalFeeInUSDC(
             IInfraStorage.CCIPToken.usdc,
-            CHAIN_SELECTOR_BASE,
+            CHAIN_SELECTOR_OPTIMISM,
             amount
         );
 
         (uint256 conceroMsgFees, uint256 ccipFees, uint256 lancaFees) = infraProxy
-            .getSrcBridgeFeesBreakdownInUsdc(CHAIN_SELECTOR_BASE, amount);
+            .getSrcBridgeFeesBreakdownInUsdc(CHAIN_SELECTOR_OPTIMISM, amount);
 
         uint256 newFees = conceroMsgFees + ccipFees + lancaFees;
         console.log("conceroMsgFees: ", conceroMsgFees);
@@ -55,15 +57,55 @@ contract BridgeTestFees is Test {
         //console.log("getFunctionsFeeInUsdc", infraProxy.getFunctionsFeeInUsdc(CHAIN_SELECTOR_BASE));
     }
 
-    function _setFees(uint64 chainSelector) internal {
+    function _setFeesBase(uint64 chainSelector) internal {
         vm.startPrank(deployer);
         //take from our prod contracts
         infraProxy.setClfPremiumFees(chainSelector, 60000000000000000);
-        infraProxy.setLastGasPrices(chainSelector, 3480453);
+        infraProxy.setLastGasPrices(chainSelector, 3453639);
 
-        infraProxy.setLatestNativeUsdcRate(2804643383934286463621);
-        infraProxy.setLatestLinkNativeRate(7132975434900616);
-        infraProxy.setLatestLinkUsdcRate(20091009336321398309);
-        infraProxy.setLastCCIPFeeInLink(chainSelector, 12289774848343018);
+        infraProxy.setLatestNativeUsdcRate(2794546316765254939452);
+        infraProxy.setLatestLinkNativeRate(7087070000000000);
+        infraProxy.setLatestLinkUsdcRate(19749620609788085970);
+
+        infraProxy.setLastCCIPFeeInLink(chainSelector, 0);
+    }
+
+    function _setFeesArb(uint64 chainSelector) internal {
+        vm.startPrank(deployer);
+        //take from our prod contracts
+        infraProxy.setClfPremiumFees(chainSelector, 75000000000000000);
+        infraProxy.setLastGasPrices(chainSelector, 3453639);
+
+        infraProxy.setLatestNativeUsdcRate(2794546316765254939452);
+        infraProxy.setLatestLinkNativeRate(7087070000000000);
+        infraProxy.setLatestLinkUsdcRate(19749620609788085970);
+
+        infraProxy.setLastCCIPFeeInLink(chainSelector, 14965818861893124);
+    }
+
+    function _setFeesOpt(uint64 chainSelector) internal {
+        vm.startPrank(deployer);
+        //take from our prod contracts
+        infraProxy.setClfPremiumFees(chainSelector, 80000000000000000);
+        infraProxy.setLastGasPrices(chainSelector, 1100409);
+
+        infraProxy.setLatestNativeUsdcRate(2794546316765254939452);
+        infraProxy.setLatestLinkNativeRate(7087070000000000);
+        infraProxy.setLatestLinkUsdcRate(19749620609788085970);
+
+        infraProxy.setLastCCIPFeeInLink(chainSelector, 11626323072779455);
+    }
+
+    function _setFeesPolygon(uint64 chainSelector) internal {
+        vm.startPrank(deployer);
+        //take from our prod contracts
+        infraProxy.setClfPremiumFees(chainSelector, 40000000000000000);
+        infraProxy.setLastGasPrices(chainSelector, 9342271);
+
+        infraProxy.setLatestNativeUsdcRate(2794546316765254939452);
+        infraProxy.setLatestLinkNativeRate(7087070000000000);
+        infraProxy.setLatestLinkUsdcRate(19749620609788085970);
+
+        infraProxy.setLastCCIPFeeInLink(chainSelector, 12373576494025416);
     }
 }
